d3ab175cc9d2e2b104084fe904196ec854c61edd
Nicolas Massé <nicolas.masse@itix.fr>
2026-05-26 18:55:22 +0000

    add support for postgres init scripts

--- cookbooks/postgresql/hooks.mk
+++ cookbooks/postgresql/hooks.mk
@@ -3,3 +3,5 @@ TARGET_POSTGRESQL_FILES = $(patsubst other/postgresql/%, $(TARGET_CHROOT)/etc/qu
 TARGET_EXAMPLE_FILES += $(TARGET_POSTGRESQL_FILES)
 $(TARGET_CHROOT)/etc/quadlets/postgresql/init.d/%.sql: other/postgresql/%.sql
 	install -m 0600 -o 10004 -g 10000 $< $@
+$(TARGET_CHROOT)/etc/quadlets/postgresql/init.d/%.sh: other/postgresql/%.sh
+	install -m 0700 -o 10004 -g 10000 $< $@
