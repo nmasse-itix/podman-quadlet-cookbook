3b2c745bdd5e4d95436508ead17e9a5a96aed8d2
Nicolas Massé <nicolas.masse@itix.fr>
2026-06-07 13:41:19 +0000

    lego hooks are not samples

--- cookbooks/lego/hooks.mk
+++ cookbooks/lego/hooks.mk
@@ -1,5 +1,5 @@
 # Lego renewal hooks
 TARGET_LEGO_FILES = $(patsubst other/lego/%.sh, $(TARGET_CHROOT)/etc/quadlets/lego/renew-hooks.d/%.sh, $(wildcard other/lego/*.sh))
-TARGET_EXAMPLE_FILES += $(TARGET_LEGO_FILES)
+TARGET_FILES += $(TARGET_LEGO_FILES)
 $(TARGET_CHROOT)/etc/quadlets/lego/renew-hooks.d/%.sh: other/lego/%.sh
 	install -D -m 0755 -o root -g root $< $@
